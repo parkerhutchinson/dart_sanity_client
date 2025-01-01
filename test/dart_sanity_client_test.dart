68be45aaca155286537693debdf92ecc4343d81e
Parker Hutchinson <hutchinsonparker@gmail.com>
2024-12-31 17:31:39 -0800

    got 1 test working. not my favorite part of writting a good package.

--- test/dart_sanity_client_test.dart
+++ test/dart_sanity_client_test.dart
@@ -1,21 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:dart_sanity_client/dart_sanity_client.dart';
+import 'package:dotenv/dotenv.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  // dotenv.get('.env.dev');
-  group('A group of tests', () {
-    // final client = DartSanityClient(
-    //   SanityConfig(
-    //     dataset: DotEnv().get('dataset'),
-    //     projectId: DotEnv().get('projectId'),
-    //   ),
-    // );
+  Directory current = Directory.current;
+  final env = DotEnv(includePlatformEnvironment: true)
+    ..load(['${current.path}/.env']);
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+  final client = DartSanityClient(SanityConfig(
+    dataset: env['dataset'] ?? '',
+    projectId: env['projectId'] ?? '',
+  ));
 
-    test('First Test', () {
-      expect('awesome', 'hello');
+  group('fetch groq', () {
+    test('client configuration', () async {
+      final String results = await client.fetch('*[_type == "todo"]{title}');
+      final resultsObject = jsonDecode(results);
+      expect(resultsObject['result'][0]["title"], "testing dart sanity client");
     });
   });
 }
